71cbdeed3ef629a96531e38cdd05d0631ba1e316
Nusret Bilallaj <netktit@gmail.com>
2022-11-18 18:57:52 +0100

    Add project files.

---
+++ Group3Web3/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Group3Web3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group3Web3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Raporti()
+        {
+            int numri = 10;
+            Studenti st1 = new Studenti("Ardit", "Grajqevci");
+            Studenti st2 = new Studenti("Muhamet", "Bersiha");
+            Studenti st3 = new Studenti("Cristiano", "Ronaldo");
+
+            List<Studenti> studentet = new List<Studenti>();
+            studentet.Add(st1);
+            studentet.Add(st2);
+            studentet.Add(st3);
+
+
+            return View(studentet);
+        }
+
+        public IActionResult MesoHtml()
+        {
+            return View();
+        }
+    }
+}
---
+++ Group3Web3/Models/Studenti.cs
@@ -0,0 +1,15 @@
+﻿namespace Group3Web3.Models
+{
+    public class Studenti
+    {
+  
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+        public Studenti(string emri, string mbiemri)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+        }
+    }
+}
